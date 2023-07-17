d7f2cb22322144c67eaa049f85d0a842b80b36cd
axelbauduin <thebasketman7@hotmail.com>
2023-07-17 16:31:28 +0200

    - Mise en place des fichiers de base pour toutes les requetes all

---
+++ Axel_ReseauSocial.Api.Domains/Queries/Contenus/GetAllContenusQuery.cs
@@ -0,0 +1,14 @@
+﻿using Axel_ReseauSocial.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Cqs.Queries;
+
+namespace Axel_ReseauSocial.Api.Domains.Queries.Contenus
+{
+    public class GetAllContenusQuery : IQuery<IEnumerable<Contenu>>
+    {
+    }
+}
